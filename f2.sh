c1bc38949275c7c3e9ca1030c6b5ba78718b7914
ammu <ammu12@gmail.com>
2024-08-06 07:03:40 +0000

    f2.sh committ

---
+++ f2.sh
@@ -0,0 +1,20 @@
+echo "enter day"
+read day
+case $day in
+	weekday) echo "monday"
+		;;
+	weekday) echo "tuesday"
+                ;;
+        weekday) echo "wednesday"
+                ;;
+	weekday) echo "thrusday"
+                ;;
+	weekday) echo "friday"
+                ;;
+	weekoof) echo "sunday"
+		;;
+	weekoof) echo "suturday"
+		;;
+	*) echo "enter a vallid day"
+case
+
